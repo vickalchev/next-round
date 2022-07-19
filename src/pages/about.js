34550577204447460ddaf5978d7f231adc1d7f89
Vic Kalchev <vic@pivotup.co>
2022-07-19 08:24:12 -0400

    Finished the second lesson on Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+const AboutPage = () => {
+    return (
+        <Layout pageTitle='About Me'>
+            
+            <p> Read more here.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage;
