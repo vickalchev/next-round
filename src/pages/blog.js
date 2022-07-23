e115885075b7b7134fec0e047eff4e6c8e7ea380
Vic Kalchev <vic@pivotup.co>
2022-07-23 13:01:48 -0400

    Added MDX transformer

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,30 +1,47 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 const BlogPage = ({data}) => {
     return (
         <Layout pageTitle='Blog Posts'>
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
+            {
+                    data.allMdx.nodes.map(node => (
+                        <article key={node.id}>
+                            <h2>{node.frontmatter.title}</h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                            <p>Updated: {node.parent.modifiedTime}</p>
+                            <MDXRenderer>
+                                {node.body}
+                            </MDXRenderer>
+                        </article>
                     ))
-                }
-            </ul>
+            }
+            
         </Layout>
     )
 }
 
 export const query = graphql `
-query {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
-      nodes {
-        name
-      }
-    }
-  }`
+    query {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+        nodes {
+            frontmatter {
+            date(formatString: "MMMM D, YYYY")
+            title
+            }
+            id
+            body
+            parent {
+            ... on File {
+                id
+                name
+                modifiedTime(formatString: "MMMM D, YYYY")
+            }
+            }
+        }
+        }
+    }`
 
 export default BlogPage;
