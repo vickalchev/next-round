b44b39c884b8e75900c95271dece9889c270853a
Vic Kalchev <vic@pivotup.co>
2022-08-02 21:40:20 -0400

    updated files

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Layout from '../../components/layout';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import {GatsbyImage, getImage} from 'gatsby-plugin-image';
 
 const BlogPost = ({data}) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
+            <GatsbyImage 
+              image={image}
+              alt={data.mdx.frontmatter.hero_image_alt}
+            />
             <p>{data.mdx.date}</p>
             <MDXRenderer>
                 {data.mdx.body}
@@ -16,15 +22,24 @@ const BlogPost = ({data}) => {
 }
 
 export const query = graphql`
-    query ($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
+query ($id: String) {
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      title
+      date(formatString: "MMMM D, YYYY")
+      hero_image_credit_link
+      hero_image_credit_text
+      hero_image_alt
+      hero_image {
+        childImageSharp {
+          gatsbyImageData
+        }
       }
-      body
     }
-  }`
+    body
+  }
+}
+`
   
 
 export default BlogPost;
